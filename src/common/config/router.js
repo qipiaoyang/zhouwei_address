2fdea5ae2c2f2296194c824d2de61ca7607035eb
齐飘洋 <qipiaoyang@outlook.com>
2020-05-01 22:08:59 +0800

    更新

--- src/common/config/router.js
+++ src/common/config/router.js
@@ -1,6 +1,7 @@
 module.exports = [
   [/\/admin\/v1\/admin(?:\/(\d+))?/, 'admin/v1/admin?id=:1', 'rest'],
   [/\/admin\/v1\/address(?:\/(\d+))?/, 'admin/v1/address?id=:1', 'rest'],
+  [/\/admin\/v1\/dept(?:\/(\d+))?/, 'admin/v1/dept?id=:1', 'rest'],
 ];
 
 
